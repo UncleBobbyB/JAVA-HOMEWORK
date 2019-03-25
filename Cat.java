853d3dd0f99ec08328ce2842d3f439b756c21905
Yingjie Jiang <yingjiejiang@YingjiedeMacBook-Pro.local>
2019-03-25 14:19:35 +0800

    -

---
+++ Cat.java
@@ -0,0 +1,59 @@
+import java.awt.*;
+
+/**
+* Title : Cat.java
+* Description: This class contains the definition of a cat.
+* Copyright : Copyright (c) 2006‐2019
+* @author Laurissa Tokarchuk
+* @version 1.0
+* @author Paula Fonseca
+* @version 1.3
+*/
+
+public class Cat {
+    // Declaration of instance variables.
+    String name, furType;
+    boolean tail;
+    Color colour;
+    int speed;
+
+    // Contrutor
+    Cat(String name, String furType, boolean tail, Color colour, int speed) {
+        this.name = name;
+        this.furType = furType;
+        this.tail = tail;
+        this.colour = colour;
+        this.speed = speed;
+    }
+
+
+
+    /** This is the sleep method for the cat. It dictates the number of
+    * minutes the cat sleeps.
+    * @param duration The number of minutes to sleep.
+    */
+    public void sleep(int duration) {
+    System.out.println("I am sleeping for " + duration + " minutes.");
+    }
+    /** This method allows the cat to run. The distance (in a straight line)
+    * the cat runs is dependent on how long the cat runs and whether or not
+    * it is running in a zigzag.
+    * @param duration The number of minutes to run.
+    * @param zigzag Whether to run in a zigzag pattern.
+    * @return int Number of metres ran.
+    */
+    public int run(int duration, boolean zigzag) {
+        System.out.println("I am running "
+        + (zigzag? "in a zigzag" : "straight")
+        + " for "
+        + duration
+        + " minutes.");
+        int distanceRun = duration * speed; // assuming speed is metres per minute
+        if (zigzag) {
+        /* When in zigzag, distance is 1/3 of what it would have been if
+        the cat was going straight. */
+        return distanceRun/3;
+        }
+        else return distanceRun;
+    }
+}
---
+++ CatTest.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+/**
+* Title      : CatTest.java
+* Description: This class contains the test class for Cat.
+* Copyright  : Copyright (c) 2006 ‐ 2019
+* @author  Laurissa Tokarchuk
+* @version 1.0
+* @author  Paula Fonseca
+* @version 1.3
+*/
+public class CatTest {
+    public static void main(String[] args) {
+        // Cat myCat = new Cat();
+        // myCat.name = "Nickos";
+        // myCat.furType = "short";
+        // myCat.tail = true;
+        // myCat.colour = Color.ORANGE;
+        // myCat.speed = 200; // in metres per minute
+
+        Cat[] cats = {new Cat("Tom", "short", true, Color.BLACK, 500), new Cat("Moggy", "long", false, Color.WHITE, 400)};
+        
+        for (int i = 0;i < 2; i++) {
+            System.out.println("name = " + "\"" + cats[i].name + "\"");
+            System.out.println("tail = " + cats[i].tail);
+            System.out.println("speed = " + cats[i].speed);
+            System.out.println("furType = " + "\"" + cats[i].furType + "\"");
+            System.out.println("colour = " + cats[i].colour);
+            System.out.println();
+        }
+        
+    }
+}
